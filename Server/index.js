81a5d1db30e7ebe5f6bf3d5ac74e70d40a9064df
olaleye <olaleyesamuel426@gmail.com>
2026-04-22 11:36:28 +0100

    Refactor server setup by removing commented code and exporting app module

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,39 @@
+
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 require('./connection');
 
 const app = express();
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.get('/', (req, res) => {
   res.json({ message: 'Server is working' });
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
+
+
+
+// const express = require('express');
+// const cors = require('cors');
+// require('dotenv').config();
+// require('./connection');
+
+// const app = express();
+// const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+
+
+// app.use(cors());
+// app.use(express.json());
+
+
+// app.get('/', (req, res) => {
+//   res.json({ message: 'Server is working' });
+// });
+
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
